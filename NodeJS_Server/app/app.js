4f4ae50e4a9b15b39f0f6bd055bcc4fe1ff3d83d
Dzmitry Astraukh <Dzmitry_Astraukh@epam.com>
2019-11-26 10:01:40 +0300

    fix error handler

--- NodeJS_Server/app/app.js
+++ NodeJS_Server/app/app.js
@@ -1,23 +1,26 @@
+const path = require('path');
 const express = require('express');
 const es6Renderer = require('express-es6-template-engine');
 
 const router = require('./router');
+
 const app = express();
 
+const viewsPath = path.join(__dirname, './views');
 app.engine('html', es6Renderer);
-app.set('views', './views');
+app.set('views', viewsPath);
 app.set('view engine', 'html');
 
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.render('errorView', {
-        locals: {
-            status: err.status || 500,
-            message: err.message || 'server error',
-        },
-    });
-});
-
 app.use('/', router);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.render('errorView', {
+    locals: {
+      status: err.status || 500,
+      message: err.message || 'server error',
+    },
+  });
+});
 
 module.exports = app;
